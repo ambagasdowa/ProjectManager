5871bfeea443aa9bade67c71354b1dd070cb50de
ambagasdowa <ambagasdowa@gmail.com>
2018-01-05 18:56:21 -0600

    Translate of Template

--- replace.sh
+++ replace.sh
@@ -1,4 +1,4 @@
-#find -name '*.html' -print0 | xargs -0 -I {} sed -ie 's///' {}
+find -name '*.html' -print0 | xargs -0 -I {} sed -ie 's/Mailus@Experts.com/contacto@idg.com/' {}
 #find -name '*.html' -print0 | xargs -0 -I {} sed -ie 's///' {}
 #find -name '*.html' -print0 | xargs -0 -I {} sed -ie 's///' {}
 #find -name '*.html' -print0 | xargs -0 -I {} sed -ie 's///' {}
